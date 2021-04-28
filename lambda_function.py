61978735f0627e2032101b0fbbc258ba99f2a98a
julienbonnerot <Julien94!*>
2021-04-28 19:58:50 +0200

    Adding sns topic arn in config file for lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,15 +1,26 @@
+import json
+
 from queries import search, display
 from client import twitter_client
 from mail import send_email
 
 # lambda entry point function
 def lambda_handler(event, context):
-    api = twitter_client()
-    user_tweets = search(api)
+    with open('json/config.json') as config_json:
+        config = json.load(config_json)
 
-    if 0 == len(user_tweets):
-        print("No tweet found.")
-    else:
-        print(str(len(user_tweets)) + " Users tweet found.")
-        body = display(user_tweets)
-        send_email("Potential YieldFarming x100", body, "")
+        api_key = config['twitter']['api_key']
+        api_secret_key = config['twitter']['api_secret_key']
+        access_token = config['twitter']['access_token']
+        access_token_secret = config['twitter']['access_token_secret']
+        topic_arn = config['sns']['topic_arn']
+
+        api = twitter_client(api_key, api_secret_key, access_token, access_token_secret)
+        user_tweets = search(api)
+
+        if 0 == len(user_tweets):
+            print("No tweet found.")
+        else:
+            print(str(len(user_tweets)) + " Users tweet found.")
+            body = display(user_tweets)
+            send_email("Potential YieldFarming x100", body, topic_arn)
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+import json
+
 from queries import search, display
 from client import twitter_client
 
-api = twitter_client()
-user_tweets = search(api)
+with open('json/config.json') as search_json:
+    config = json.load(search_json)
 
-if 0 == len(user_tweets):
-    print("No tweet found.")
-else:
-    print(str(len(user_tweets)) + " Users tweet found.")
-    display(user_tweets)
+    api_key = config['twitter']['api_key']
+    api_secret_key = config['twitter']['api_secret_key']
+    access_token = config['twitter']['access_token']
+    access_token_secret = config['twitter']['access_token_secret']
+
+    api = twitter_client(api_key, api_secret_key, access_token, access_token_secret)
+    user_tweets = search(api)
+
+    if 0 == len(user_tweets):
+        print("No tweet found.")
+    else:
+        print(str(len(user_tweets)) + " Users tweet found.")
+        display(user_tweets)
